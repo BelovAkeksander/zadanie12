f92ff972d48d182dd3945ab45a0ce0a38fb20770
BelovAkeksander <147313344+BelovAkeksander@users.noreply.github.com>
2024-06-08 11:17:04 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace YourNamespace
         [ColumnName("ImageSource"), LoadColumn(0)]
         public string ImageSource { get; set; }
 
-        // Добавьте другие свойства модели ввода, если это необходимо
+        
     }
 
     public class ModelOutput
@@ -18,7 +18,7 @@ namespace YourNamespace
         [ColumnName("PredictedLabel")]
         public string PredictedLabel { get; set; }
 
-        // Добавьте другие свойства модели вывода, если это необходимо
+   
     }
 
     class Program
@@ -27,11 +27,11 @@ namespace YourNamespace
         {
             try
             {
-                // Загружаем модель
+               
                 MLContext mlContext = new MLContext();
                 string modelPath = Path.Combine(AppContext.BaseDirectory, "models", "C:\\Users\\Alex\\Desktop\\zadanie\\MLModel1.mlnet"); // Проверьте правильный формат файла
 
-                // Проверьте существование файла перед загрузкой
+              
                 if (!File.Exists(modelPath))
                 {
                     throw new FileNotFoundException($"Модель не найдена по пути: {modelPath}");
@@ -43,19 +43,19 @@ namespace YourNamespace
                     mlModel = mlContext.Model.Load(stream, out var modelInputSchema);
                 }
 
-                // Создаем прогнозировщик
+           
                 var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
 
-                // Загружаем изображение для теста
+              
                 var imageData = new ModelInput()
                 {
-                    ImageSource = "C:\\Users\\Alex\\Desktop\\dataset\\13\\icons8-13-48.png" // Убедитесь, что путь к изображению правильный
+                    ImageSource = "C:\\Users\\Alex\\Desktop\\dataset\\13\\icons8-13-48.png"
                 };
 
-                // Делаем предсказание
+               
                 var prediction = predEngine.Predict(imageData);
 
-                // Выводим результат
+            
                 Console.WriteLine($"Predicted digit: {prediction.PredictedLabel}");
             }
             catch (Exception ex)
